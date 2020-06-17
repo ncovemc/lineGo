ffe715bf3658b74d9f4246041dd27768e4933096
sakura <56917703+Ch31212y@users.noreply.github.com>
2020-06-17 16:06:51 +0900

    Create example.py

---
+++ example.py
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"./line"
+)
+
+func main() {
+	cl = line.Line("token", "IOS")
+	fetchOp(cl)
+}
+
+func fetchOp(cl line.Clinet) {
+	for {
+		ops := cl.fetchOperations(cl.ctx, cl.revision, count)
+		for op := range ops {
+			bot(op)
+			if cl.revision < op.Revision {
+				cl.revision = op.Revision
+			}
+		}
+	}
+}
+
+func bot(op line.Operation) {
+	switch op.Type {
+	case 26:
+		msg := op.message
+		if msg.text == "test" {
+			cl.SendMessage(msg.to, "ok")
+		}
+	}
+}
